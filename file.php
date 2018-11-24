cbbd7165e30d829d7904d3c94a4c468cafd079bc
arafatomer <arafatomer66@gmail.com>
2018-11-24 13:46:26 +0600

    commit

---
+++ file.php
@@ -0,0 +1,58 @@
+<?php
+
+  header('Access-Control-Allow-Origin :*');
+
+
+  header('Content-Type : application/json');
+?>
+
+
+{
+    "data": [
+        {
+            "id"    : 1 ,
+            "image" : "a" ,
+            "title" : "Rice" ,
+            "description" : "hellow , this is an app" ,
+            "content" : "hey"
+        },
+        {
+            "id"    : 2 ,
+            "image" : "a" ,
+            "title" : "fish" ,
+            "description" : "hellow , this is an app" ,
+            "content" : "hey"
+        },
+        {
+            "id"    : 3 ,
+            "image" : "a" ,
+            "title" : "vegetables" ,
+            "description" : "hellow , this is an app" ,
+            "content" : "hey"
+        },
+        {
+            "id"    : 4 ,
+            "image" : "a" ,
+            "title" : "Meat" ,
+            "description" : "hellow , this is an app" ,
+            "content" : "hey"
+        },
+        {
+            "id"    : 5 ,
+            "image" : "a" ,
+            "title" : "Egg" ,
+            "description" : "hellow , this is an app" ,
+            "content" : "hey"
+        },
+        {
+            "id"    : 6 ,
+            "image" : "a" ,
+            "title" : "Chicken" ,
+            "description" : "hellow , this is an app" ,
+            "content" : "hey"
+        }
+    ]
+}
+
+
+<!-- php -S localhost:1234 -->
